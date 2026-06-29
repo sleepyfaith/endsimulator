78a62fdab52cbada7053ab938c899c936921bc97
sleepyfaith <67561809+sleepyfaith@users.noreply.github.com>
2026-06-29 17:36:38 +0100

    BPos instead of CPos

--- src/structures/EndGateway.kt
+++ src/structures/EndGateway.kt
@@ -1,15 +1,15 @@
 package lgbt.faith.structures
 
 import lgbt.faith.biome.BiomeSource
-import lgbt.faith.block.CPos
+import lgbt.faith.block.BPos
 import lgbt.faith.rand.Rand
 
 class EndGateway() {
     val linkedGateways =  arrayOf(
-        CPos(96, 0), CPos(91, 29), CPos(77, 56), CPos(56, 77), CPos(29, 91),
-        CPos(-1, 96), CPos(-30, 91), CPos(-57, 77), CPos(-78, 56), CPos(-92, 29),
-        CPos(-96, -1), CPos(-92, -30), CPos(-78, -57), CPos(-57, -78), CPos(-30, -92),
-        CPos(0, -96), CPos(29, -92), CPos(56, -78), CPos(77, -57), CPos(91, -30)
+        BPos(96, 0), BPos(91, 29), BPos(77, 56), BPos(56, 77), BPos(29, 91),
+        BPos(-1, 96), BPos(-30, 91), BPos(-57, 77), BPos(-78, 56), BPos(-92, 29),
+        BPos(-96, -1), BPos(-92, -30), BPos(-78, -57), BPos(-57, -78), BPos(-30, -92),
+        BPos(0, -96), BPos(29, -92), BPos(56, -78), BPos(77, -57), BPos(91, -30)
     )
 
     fun getEndIslandGatewayOrder(biomeSource: BiomeSource): IntArray {
